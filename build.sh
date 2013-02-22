39fd3c3a4acba670d654622e814e991275710009
Dominic Tarr <dominic.tarr@gmail.com>
2013-02-22 21:42:26 +1300

    use css-colab to edit css

--- build.sh
+++ build.sh
@@ -2,7 +2,13 @@
 browserify css.js -o static/css-colab.js --debug
 
 {
-  echo '<!DOCTYPE HTML><html><body></body><script>'
+  echo '<!DOCTYPE HTML><html><head>'
+  echo '<link rel=stylesheet href=./style.css >'
+  echo '<script'\
+  'src=http://rumoursdb.com/examples/css-colab/css-colab.js>'\
+  '></script>'
+  echo '<script>CssColab("CSS-LAB")</script>'
+  echo '</head><body></body><script>'
   browserify client.js --debug --exports require || exit 1
   echo '</script></html>'
 } > static/editor.html
