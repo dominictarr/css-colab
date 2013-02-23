c969513c45dd01cb57cef0f4acc63d098cc0fb5c
Dominic Tarr <dominic.tarr@gmail.com>
2013-02-23 20:17:17 +1300

    update style

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ browserify css.js -o static/css-colab.js --debug
 {
   echo '<!DOCTYPE HTML><html><head>'
   echo '<link rel=stylesheet href=./style.css >'
-  echo '<script'\
-  'src=http://rumoursdb.com/examples/css-colab/css-colab.js>'\
-  '></script>'
+#  echo '<script'\
+#  'src=http://rumoursdb.com/examples/css-colab/css-colab.js>'\
+#  '></script>'
   echo '<script>if(window.location.hash != '#CSS-LAB') CssColab("CSS-LAB")</script>'
   echo '</head><body></body><script>'
   browserify client.js --debug --exports require || exit 1
